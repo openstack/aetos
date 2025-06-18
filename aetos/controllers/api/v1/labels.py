0a979266f6e0016ab52ad4ecb29c244a63f581ab
Jaromir Wysoglad <jwysogla@redhat.com>
2025-06-18 05:41:24 -0400

    Allow match[] parameter in label and labels
    This will allow users to restrict the amount of values returned.
    For example admins will be able to restrict the labels to just
    one project similarly to what's being done automatically when
    a request is sent from non-admin user.
    
    Also modify similar endpoints slightly to follow the same pattern
    of having *args, **kwargs arguments and checking the length of
    args.
    
    Change-Id: I408a5d276a873912f6d0845fd985d44ceda4b95f

--- aetos/controllers/api/v1/labels.py
+++ aetos/controllers/api/v1/labels.py
@@ -13,12 +13,13 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import json
+
 from observabilityclient import rbac as obsc_rbac
 from oslo_log import log
 import pecan
 from webob import exc
-from wsme import types as wtypes
-import wsmeext.pecan as wsme_pecan
+from wsme import exc as wsme_exc
 
 from aetos.controllers.api.v1 import base
 from aetos import rbac
@@ -27,8 +28,8 @@ LOG = log.getLogger(__name__)
 
 
 class LabelsController(base.Base):
-    @wsme_pecan.wsexpose(wtypes.text)
-    def get(self):
+    @pecan.expose(content_type='application/json')
+    def get(self, *args, **kwargs):
         """Labels endpoint"""
         target = {"project_id": pecan.request.headers.get('X-Project-Id')}
         try:
@@ -46,18 +47,48 @@ class LabelsController(base.Base):
                 "Received a low privilege request for the labels endpoint"
             )
 
+        status_code = 200
+
+        if len(args) != 0:
+            pecan.response.status = 404
+            return json.dumps("page not found")
+
+        matches = kwargs.get('match[]', [])
+        if not isinstance(matches, list):
+            matches = [matches]
+
         self.create_prometheus_client(pecan.request.cfg)
 
         if privileged:
-            result = self.prometheus_get("labels")
+            LOG.debug("Matches sent to prometheus: %s", str(matches))
+            try:
+                result = self.prometheus_get("labels", {"match[]": matches})
+            except wsme_exc.ClientSideError as e:
+                status_code = e.code
+                result = e.msg
         else:
             promQLRbac = obsc_rbac.PromQLRbac(
                 self.prometheus_client,
                 target['project_id']
             )
-            result = self.prometheus_get(
-                "labels", {"match[]": promQLRbac.append_rbac_labels('')}
-            )
+            modified_matches = []
+            if matches == []:
+                modified_matches = promQLRbac.append_rbac_labels('')
+            else:
+                for match in matches:
+                    modified_matches.append(
+                        promQLRbac.modify_query(match)
+                    )
+            LOG.debug("Matches sent to prometheus: %s", str(modified_matches))
+            try:
+                result = self.prometheus_get(
+                    "labels", {"match[]": modified_matches}
+                )
+            except wsme_exc.ClientSideError as e:
+                status_code = e.code
+                result = e.msg
 
         LOG.debug("Data received from prometheus: %s", str(result))
-        return result
+
+        pecan.response.status = status_code
+        return json.dumps(result)
